9fc0a6ca1d0c7e08ad28377bd79d5b2ea61c80e9
ZulyVargas <zuly.vargas@mail.escuelaing.edu.co>
2021-08-19 09:41:32 -0500

    Modificando App

--- Patterns/src/main/java/edu/eci/cvds/patterns/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -8,6 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        if (args.length==0) System.out.println( "Hello World!" );
+        if (args.length>0) {
+        	String saludo="Hello";
+        	for (int i=0; i<args.length;i++) {
+        		saludo = saludo+" "+args[i];
+        	}
+        	saludo = saludo +"!";
+            System.out.println(saludo);
+        }
     }
 }
