c8ed2e9238a9b37b7c92ec7312e777fc9e262c69
CamiloPichimata <camilo.pichimata@mail.escuelaing.edu.co>
2021-08-25 14:53:32 -0500

    Se documentaron las clases

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -2,8 +2,17 @@ package edu.eci.cvds.patterns.shapes;
 
 import edu.eci.cvds.patterns.shapes.concrete.*;
 
+/**
+ * ShapeFactory class that implements the factory pattern using the switch-case statement  
+ */
 public class ShapeFactory {
-
+	
+	/**
+	 * Static method that is responsible for creating a figure taking into account the given type
+	 * @param type Type of the new figure to create
+	 * @return returns an instance of the given type
+	 * @throws IllegalArgumentException
+	 */
 	public static Shape create (RegularShapeType type) throws IllegalArgumentException{
 		
 		switch (type) {
